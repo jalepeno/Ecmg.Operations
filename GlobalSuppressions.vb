658241abedf20f79eec317dab1785f3a174fbbc0
magbender <58702100+jalepeno@users.noreply.github.com>
2025-01-17 14:04:06 -0600

    Add files via upload

---
+++ GlobalSuppressions.vb
@@ -0,0 +1,16 @@
+﻿' This file is used by Code Analysis to maintain SuppressMessage
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given
+' a specific target and scoped to a namespace, type, member, etc.
+
+Imports System.Diagnostics.CodeAnalysis
+
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Operations.ProcessResults.Contains(Operations.IProcessResult)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Operations.ProcessResults.Add(Operations.IProcessResult)")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Operations.ProcessResults.Remove(Operations.IProcessResult)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Operations.OperationResults.Add(Operations.IOperableResult)")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Operations.OperationResults.Contains(Operations.IOperableResult)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Operations.OperationResults.Remove(Operations.IOperableResult)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Operations.OperationExtensions.Contains(Operations.Extensions.IOperationExtension)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Operations.OperationExtensions.Remove(Operations.Extensions.IOperationExtension)~System.Boolean")>
+<Assembly: SuppressMessage("Design", "CA1061:Do not hide base class methods", Justification:="<Pending>", Scope:="member", Target:="~M:Operations.OperationExtensions.Add(Operations.Extensions.IOperationExtension)")>
